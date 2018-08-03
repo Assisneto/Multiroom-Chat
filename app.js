b01eccfe23819d07e9266bfbe29ec6128b8d36c3
Assisneto <netodamasceno12@gmail.com>
2018-08-03 11:16:11 -0300

    settign

---
+++ app.js
@@ -0,0 +1,5 @@
+let app = require('./config/express')
+
+app.listen(3000,()=>{
+    console.log('Servidor online na porta 3000')
+});
---
+++ config/express.js
@@ -0,0 +1,25 @@
+let express = require('express');
+
+let consign = require('consign');
+
+let bodyParser = require('body-parser');
+
+let expressValidator = require('express-validator');
+
+let app = express();
+
+app.set('view engine', 'ejs');
+app.set('view','./app/views');
+
+app.use(express.static('./app/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(expressValidator());
+
+consign()
+    .include('app/routes')
+    .then('app/models')
+    .then('app/controllers')
+    .into(app);
+    
+module.exports = app;
+
